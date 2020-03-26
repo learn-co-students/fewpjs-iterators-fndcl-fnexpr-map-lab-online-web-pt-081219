07dfa0b64ff88cc995bdcb8311417352fe4c3aee
saramccombs <theSaraMcCombs@gmail.com>
2020-03-26 14:47:31 -0400

    Done.

--- index.js
+++ index.js
@@ -12,13 +12,19 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  // map each sentence
+  // map each sentence in the tutorials array
   return tutorials.map( sentence => {
-    // 
+    // split each sentence into an array of words
     let words = sentence.split(' ')
+    // map each word in the words array
+    // select the first letter of the word via .charAt(0)
+    // capitalize the lett via .toUpperCase()
+    // add the rest of the characters from word via .slice(1)
     let capitalizedWords =
       words.map( word => word.charAt(0).toUpperCase() + word.slice(1) )
+    // rejoin all the words to recereate the sentence
     let response = capitalizedWords.join(' ')
+    // returns response to the map function
     return response
   })
 }
