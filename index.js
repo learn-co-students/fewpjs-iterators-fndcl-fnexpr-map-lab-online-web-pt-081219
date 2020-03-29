1133ec605c63a0d524f426cf454273901e82b1a0
James <jamescscalise@gmail.com>
2020-03-29 15:18:05 -0500

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  let newTutorials = tutorials.map(function(tutorial){
+    tutorial = tutorial.split(' ')
+             .map(function(word) {
+      return (word.charAt(0).toUpperCase() + word.slice(1));
+    });
+   return tutorial.join(' ');
+  });
+  return newTutorials;
 }
