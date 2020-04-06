8ad8f353c02be6d18a82ceed11f848b4c413d688
Maria Le <hp.mars.le@gmail.com>
2020-04-06 13:46:37 -0500

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  //captialize each title in the tutorials array
+  return tutorials.map(title => {
+    // split each title into an array of its words
+    let words = title.split(" ")
+    // map each word and capitalize the first letter of each word with .charAt(0) and concat with rest of the word via .slice(1)
+    let capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    // return capitalized words back into the title string
+    return capitalizedWords.join(' ');
+  })
 }
